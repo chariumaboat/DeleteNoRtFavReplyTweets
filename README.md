74b3428363d83d09d21b98596af8c632842eb31c
natsukawadaisuki <60532865+natsukawadaisuki@users.noreply.github.com>
2020-05-28 11:32:08 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# DeleteNoRtFavReplyTweets
+# Script to delete tweets every day
+
+Leave only your high quality tweets.
+Run daily with AWS Lambda.
+
+- Tweet to be deleted
+  - No RT Tweets
+  - No Favorite Tweets
+  - No Reply Tweets
+
+## How to deploy
+- Precondition
+  - Python
+  - AWS CLI
+  - Twitter API Key
+
+- Includes tweepy(External module) in package
+
+```Powershell
+pip install tweepy --target ./code
+```
+
+- Packaging
+
+```Powershell
+aws cloudformation package --s3-bucket $YourBucketName `
+--template-file lambda.yml `
+--output-template-file lambda-packaged.yml
+```
+
+- Deploy
+
+```Powershell
+aws cloudformation deploy `
+--template-file lambda-packaged.yml `
+--stack-name $YourStackName `
+--parameter-overrides AccessSecret=$YourAccessSecret `
+ConsumerKey=$YourConsumerKey ` 
+ConsumerSecret=$YourConsumerSecret ` 
+AccessKey=$YourAccessKey `
+--capabilities CAPABILITY_NAMED_IAM
+```
---
+++ code/noRtFavTweetDelete.py
@@ -0,0 +1,50 @@
+import tweepy
+import os
+
+# API Key
+consumer_key = os.environ['ConsumerKeyVar']
+consumer_secret = os.environ['ConsumerSecretVar']
+access_key = os.environ['AccessKeyVar']
+access_secret = os.environ['AccessSecretVar']
+
+# Tweepy Auth
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+noFavRtTweet = []
+mentionTweet = []
+noDeleteTweet = []
+
+def lambda_handler(event, context):
+    # No Favorite No RT Tweet
+    print("==========No Favorite No RT Tweet==========")
+    for tweet in tweepy.Cursor(api.user_timeline,exclude_replies = True).items():
+        if tweet.favorite_count == 0 and tweet.retweet_count == 0:
+            print(tweet.id,tweet.created_at,tweet.text.replace('\n',''))
+            noFavRtTweet.append(tweet.id)
+
+    # Reply Tweet
+    print("==========Reply Tweet==========")
+    for mentions in tweepy.Cursor(api.mentions_timeline).items():
+        print(mentions.id,mentions.created_at,mentions.text.replace('\n',''))
+        mentionTweet.append(mentions.in_reply_to_status_id)
+
+    print("==========No Favorite No RT Reply Tweet==========")
+    # No Favorite No RT Reply Tweet
+    for tweet in noFavRtTweet:
+        for reptw in mentionTweet:
+            if tweet == reptw:
+                print(api.get_status(tweet).id,api.get_status(tweet).created_at,api.get_status(tweet).text.replace('\n',''))
+                noDeleteTweet.append(tweet)
+
+    # Extraction Delete Tweet
+    print("==========Extraction Delete tweet==========")
+    perfectList = set(noFavRtTweet) ^ set(noDeleteTweet)
+    print(list(perfectList))
+
+    # Delete Tweet
+    print("==========delete tweet==========")
+    for deltw in perfectList:
+        print(api.get_status(deltw).id,api.get_status(deltw).created_at,api.get_status(deltw).text)
+        api.destroy_status(deltw)
